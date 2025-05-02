f481ecdf5db111c771df0a0e6cf8c873b2500f9e
Duhan <demirduhan1@gmail.com>
2025-05-02 22:29:32 +0300

    Hareket

--- Assets/Scripts/DedektifController.cs
+++ Assets/Scripts/DedektifController.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class DedektifController : MonoBehaviour
 {
-    [Header("Movement Settings")] public float walkSpeed = 5f;
+    [Header("Movement Settings")] 
+    public float walkSpeed = 4f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
 
+    private Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    
     private CharacterController controller;
 
     void Start()
@@ -17,14 +27,50 @@ public class DedektifController : MonoBehaviour
 
     void Update()
     {
-        // Get input
+        CheckGround();
+        
+        Movement();
+        // Zıplama
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        // Yerçekimi uygulama
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Movement()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // Calculate movement relative to player's facing direction
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = (transform.right * x + transform.forward * z);
+        move.y = 0f;
+        
+        controller.Move(move * (walkSpeed * Time.deltaTime));
+    }
+/*
+    private void Jump()
+    {
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+        if (jump)
+        {
+            controller.velocity.y(jumpHeight * -2f * gravity);
+        }
+        
+    }
+    */
 
-        // Apply movement
-        controller.Move(move * walkSpeed * Time.deltaTime);
+    private void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Yere yapıştır
+        }
+
     }
 }
